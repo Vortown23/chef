416fc61f86a45b2dbcdc5f3a00caa309e3a1559c
Jay Mundrawala <jdmundrawala@gmail.com>
2015-03-07 17:51:34 -0800

    Reorganize nesting for powershell classes

--- lib/chef/util/powershell/cmdlet_result.rb
+++ lib/chef/util/powershell/cmdlet_result.rb
@@ -18,7 +18,9 @@
 
 require 'chef/json_compat'
 
-class Chef::Util::Powershell
+class Chef
+class Util
+class Powershell
   class CmdletResult
     attr_reader :output_format
 
@@ -43,4 +45,6 @@ class Chef::Util::Powershell
       @succeeded = @status.status.exitstatus == 0
     end
   end
+end
+end
 end
